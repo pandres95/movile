515588048359f159af53d6ff5e2e99dde8e1d6c7
Pablo Andrés Dorado Suárez <pandres95@gentebool.co>
2014-01-04 00:05:44 -0500

    OpenData model workaround: Identifying json_beautify for PHP versions under 5.4.0

--- models/model.municipios.php
+++ models/model.municipios.php
@@ -2,17 +2,23 @@
 
 class municipiosModel extends Model{
     
-    public function municipiosModel(){
-        
-    }
+    public function municipiosModel(){}
     
     public function getMunicipiosJSON($data){
+        
         $sql = '';
+        
         if($data){
-            $sql = "SELECT movile_municipios.id as codigo, nombre, nombre_departamento, movile_municipios.longitud as longitud, movile_municipios.latitud as latitud FROM movile_municipios, movile_departamentos WHERE departamento = movile_departamentos.id AND movile_departamentos.id = $data";
+            $sql = "SELECT movile_municipios.id as codigo, nombre, nombre_departamento, " .
+                "movile_municipios.longitud as longitud, movile_municipios.latitud as latitud ".
+                "FROM movile_municipios, movile_departamentos WHERE departamento = movile_departamentos.id ".
+                "AND movile_departamentos.id = $data";
         } else {
-            $sql = "SELECT movile_municipios.id as codigo, nombre, nombre_departamento, movile_municipios.longitud as longitud, movile_municipios.latitud as latitud FROM movile_municipios, movile_departamentos WHERE departamento = movile_departamentos.id";
+            $sql = "SELECT movile_municipios.id as codigo, nombre, nombre_departamento, ".
+                "movile_municipios.longitud AS longitud, movile_municipios.latitud as latitud ".
+                "FROM movile_municipios, movile_departamentos WHERE departamento = movile_departamentos.id";
         }
+        
         $municipios = Model::query($sql);
         
         if(count($municipios) > 0){
@@ -41,10 +47,15 @@ class municipiosModel extends Model{
                 }
                 $res = array("municipios"=>$res);
             }
-            return json_encode($res, JSON_PRETTY_PRINT);
         }else{
-            return json_encode(array('error' => true), JSON_PRETTY_PRINT);
+            $res = array('error' => true);
         }
+        if(PHP_VERSION_ID < 50400){
+            return pretty_json(json_encode($res), "\n", "    ");
+        } else {
+            return json_encode($res, JSON_PRETTY_PRINT);
+        }
+        
     }
     
 }
