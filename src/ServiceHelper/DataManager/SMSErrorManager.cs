258e9353e48a50c51264d362d628f36c9f90679f
Pablo Andrés Dorado Suárez <pandres95@gentebool.co>
2014-01-04 18:02:07 -0500

    Documentation updated

---
+++ src/ServiceHelper/DataManager/SMSErrorManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace ServiceHelper.DataManager
+{
+    public class SMSErrorManager
+    {
+
+        /// <summary>
+        /// Conexión a la base de datos
+        /// </summary>
+        DbConnection dbConn;
+
+        /// <summary>
+        /// Constructor de la conexión de base de datos
+        /// </summary>
+        /// <param name="host">Servidor host para la conexión.</param>
+        /// <param name="uName">Nombre de usuario para la conexión</param>
+        /// <param name="pass">Constraseña de conexión</param>
+        /// <param name="db">Base de datos a conectarse.</param>
+        public SMSErrorManager(String host, String uName, String pass, String db)
+        {
+            try
+            {
+                dbConn = DatabaseConnection.Conectar(host, uName, pass, db);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Busca si el usuario <paramref name="celular"/> tiene preguntas activas por responder.
+        /// </summary>
+        /// <param name="celular">Número de celular del usuario a buscar.</param>
+        /// <returns>Un valor <see cref="bool"/> indicando si el usuario tiene preguntas activas para responder o no.</returns>
+        public void LogSMSError(String celular)
+        {
+            
+            try
+            {
+                
+                DbCommand dbComm = new MySqlCommand("INSERT INTO sms_fallidos(celular, fecha) VALUES(@cellphone, "
+                    + "CURRENT_TIMESTAMP);");
+                dbComm.Connection = dbConn;
+
+                DbParameter cPhone = new MySqlParameter("cellphone", MySqlDbType.VarChar);
+                cPhone.Value = celular;
+
+                dbComm.Parameters.Add(cPhone);
+
+                dbConn.Open();
+                dbComm.ExecuteNonQuery();
+                dbConn.Close();
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+    }
+}
---
+++ src/ServiceHelperTest/SMSErrorManagerTest.cs
@@ -0,0 +1,83 @@
+﻿using ServiceHelper.DataManager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace QuestionsTest
+{
+    
+    
+    /// <summary>
+    ///Se trata de una clase de prueba para SMSErrorManagerTest y se pretende que
+    ///contenga todas las pruebas unitarias SMSErrorManagerTest.
+    ///</summary>
+    [TestClass()]
+    public class SMSErrorManagerTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Obtiene o establece el contexto de la prueba que proporciona
+        ///la información y funcionalidad para la ejecución de pruebas actual.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Atributos de prueba adicionales
+        // 
+        //Puede utilizar los siguientes atributos adicionales mientras escribe sus pruebas:
+        //
+        //Use ClassInitialize para ejecutar código antes de ejecutar la primera prueba en la clase 
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup para ejecutar código después de haber ejecutado todas las pruebas en una clase
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize para ejecutar código antes de ejecutar cada prueba
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup para ejecutar código después de que se hayan ejecutado todas las pruebas
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Una prueba de LogSMSError
+        ///</summary>
+        [TestMethod()]
+        public void LogSMSErrorTest()
+        {
+            string host = "localhost"; // TODO: Inicializar en un valor adecuado
+            string uName = "shorty_your658"; // TODO: Inicializar en un valor adecuado
+            string pass = "g2sdxPS2i7"; // TODO: Inicializar en un valor adecuado
+            string db = "shorty_movile"; // TODO: Inicializar en un valor adecuado
+            SMSErrorManager target = new SMSErrorManager(host, uName, pass, db); // TODO: Inicializar en un valor adecuado
+            string celular = "3014599967"; // TODO: Inicializar en un valor adecuado
+            target.LogSMSError(celular);
+            Assert.Inconclusive("Un método que no devuelve ningún valor no se puede comprobar.");
+        }
+    }
+}
