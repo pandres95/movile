0507312ad701bfc1f0a593d9445b5a8880c5cb56
Pablo Andrés Dorado Suárez <pandres95@gentebool.co>
2014-01-11 05:21:30 -0500

    Changed schema and added new views and stored procedures

--- PruebaSMS.sql
+++ PruebaSMS.sql
@@ -1,11 +1,13 @@
 /* Inserción de datos de prueba */
 SELECT * FROM movile_encuestas;
 INSERT INTO movile_encuestas (nombre) VALUES ('Encuesta de satisfaccion de MySQL');
+
 SELECT * FROM movile_preguntas;
 INSERT INTO movile_preguntas (id_encuesta, texto) VALUES (1, 'Como se siente usando MySQL Workbench?');
 INSERT INTO movile_preguntas (id_encuesta, texto) VALUES (1, 'Que piensa del uso de MySQL Workbench?');
-SELECT * FROM movile_respuestas;
 
+DELETE FROM movile_respuestas;
+SELECT * FROM movile_respuestas;
 INSERT INTO movile_respuestas (id_pregunta, texto, numeral) VALUES (1, 'Excelente', 1);
 INSERT INTO movile_respuestas (id_pregunta, texto, numeral) VALUES (1, 'Regular', 2);
 INSERT INTO movile_respuestas (id_pregunta, texto, numeral) VALUES (1, 'Mal', 3);
@@ -24,6 +26,7 @@ SELECT @res AS r;
 
 /* Responde una pregunta con el numeral dado */
 CALL responder_pregunta('3014599967', 2, @res);
+CALL responder_pregunta('3007261215', 1, @res);
 SELECT @res as r;
 
 /* Pregunta si hay más preguntas para responder en la encuesta activa del usuario */
@@ -31,7 +34,7 @@ CALL hay_siguiente_pregunta('3014599967', @res);
 SELECT @res as r;
 
 /* Agrega la siguiente pregunta al usuario */
-CALL agregar_siguiente_pregunta('3014599967', res);
+CALL agregar_siguiente_pregunta('3014599967', @res);
 SELECT @res AS r;
 
 /* Invalida las preguntas que fueron abiertas previamente y no resueltas */
@@ -55,6 +58,7 @@ INSERT INTO sms_fallidos(celular, fecha) VALUES ('3014599967', CURRENT_TIMESTAMP
 SELECT (SELECT COUNT(id) AS cuenta FROM respuestas_usuarios LIMIT 1) + (SELECT COUNT(id) AS cuenta FROM sms_fallidos LIMIT 1) AS cuenta;
 
 INSERT INTO movile_usuarios VALUES (NULL, 'johasalinasq', 'Johana Salinas Q.', MD5('joahasalinasq'), 2, 1);
+INSERT INTO movile_usuarios VALUES (NULL, 'jspaz', 'Joan Paz', MD5('jspaz'), 2, 1);
 
 TRUNCATE movile_menu;
 
@@ -87,9 +91,52 @@ SELECT *
 FROM movile_encuestas
 ORDER BY id DESC;
 
+DROP VIEW respuestas_por_departamento;
+
+CREATE VIEW `respuestas_por_departamento` AS
+SELECT r.id, d.id as depto
+FROM respuestas_usuarios r,
+	movile_celulares c, 
+	movile_municipios m, 
+	movile_departamentos d
+WHERE r.celular = c.celular 
+	AND c.municipio = m.id 
+	AND m.departamento = d.id 
+	AND r.estado = 2;
+
+SELECT nombre_departamento FROM movile_departamentos WHERE id = '11';
+SELECT nombre_departamento FROM movile_departamentos WHERE id = '73';
+
+CALL respuestas_por_departamento();
+
 SELECT id, nombre FROM encuestas_por_ciudad WHERE municipio = '73001';
 SELECT * FROM ultimas_encuestas_agregadas;
 
 DROP VIEW sms_enviados_por_municipio;
 
 SELECT count(id) FROM sms_enviados_por_municipio WHERE municipio = '11001';
+
+CREATE VIEW `sms_enviados_por_municipio` AS
+SELECT r.celular, 
+	m.id as municipio 
+FROM respuestas_usuarios r, 
+	movile_celulares c, 
+	movile_municipios m
+WHERE r.celular = c.celular 
+	AND c.municipio = m.id;
+
+CREATE TABLE IF NOT EXISTS `shorty_movile`.`sms_enviados` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `celular` CHAR(10) NOT NULL,
+  `mensaje` TEXT NOT NULL,
+  `fecha` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_sms_enviados_movile_celulares1_idx` (`celular` ASC),
+  CONSTRAINT `fk_sms_enviados_movile_celulares1`
+    FOREIGN KEY (`celular`)
+    REFERENCES `shorty_movile`.`movile_celulares` (`celular`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CALL insertar_celular('31212341','73001');
